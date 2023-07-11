c9aca487822869f1d53abc766764f96bd9855e21
nitheeshkm <nitheesh2013@gmail.com>
2023-07-11 17:09:52 -0400

    add y444to422

--- cocotbext/vidio/csc.py
+++ cocotbext/vidio/csc.py
@@ -5,6 +5,9 @@ from .colorconv import r2y_601, r2y_709, r2y_2020
 from .constants import Standard
 
 def rgb2yuv(rgb, standard):
+    """
+    returns matrix with shape (m*n, 3)
+    """
     if standard == Standard.BT601:
         r2y = r2y_601()
     elif standard == Standard.BT709:
@@ -20,3 +23,16 @@ def rgb2yuv(rgb, standard):
 
 def yuv2rgb():
     pass
+
+def y444to422(y444):
+    """
+    returns matrix with shape (m*n, 3)
+    """
+    y422 = np.zeros((y444.shape[0], y444.shape[1]), dtype=np.uint16)
+    y422 = y444
+    for i in range(y444.shape[0]):
+        if not(i%2):
+            y422[i][0] = y444[i][0]
+            y422[i][1] = (y444[i][1] // 2) + (y444[i+1][1] // 2)
+            y422[i][2] = (y444[i][2] // 2) + (y444[i+1][2] // 2)
+    return y422.T
